173687492a556a3ded37b637b154f6956d8c46ee
Isaac <isaac.cv123@gmail.com>
2026-04-27 23:25:29 -0400

    Creacion de clase Sprite

---
+++ src/Sprite.java
@@ -0,0 +1,10 @@
+public class Sprite extends Bebida {
+    public Sprite(int serie) {
+        super(serie);
+    }
+
+    @Override
+    public String Beber() {
+        return new String("Sabor Sprite");
+    }
+}
