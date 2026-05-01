5b0b56fa4c6e7ac625148d8577e019ab2aea48ad
Eltigre <bastiton@gmail.com>
2026-05-01 01:29:59 -0400

    Mejoras en la documentacion de la clase Producto

--- src/Producto.java
+++ src/Producto.java
@@ -1,13 +1,28 @@
+/**
+ * se define la clase abstracta Producto
+ * que servira como base de varibales y metodos para las demas sub clases de esta
+ */
 abstract class Producto {
     private int serie;
-
+    /**
+     * Se crea un producto su numero de serie
+     * @param serie dek producto
+     */
     public Producto(int serie) {
         this.serie = serie;
     }
 
+    /**
+     * se obtiene el numero de serie
+     * @return numero de serie
+     */
     public int getSerie(){
         return this.serie;
     }
 
+    /**
+     * Metodo que sirve como base para que cada Producto retorne su propio sabor
+     * @return sabor del producto escogido
+     */
     public abstract String Sabor();
 }
