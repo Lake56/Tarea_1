2aeeb79266ff7e3b6c85358d6cdf3cd4fd336c35
Eltigre <bastiton@gmail.com>
2026-05-01 01:16:19 -0400

    Mejoras en la documentacion y arreglo de error en enum

--- src/PreciosProductos.java
+++ src/PreciosProductos.java
@@ -1,21 +1,36 @@
+/**
+ * Se crea la clase enum que contiene los valores y codigos verificadores de producto
+ * Ademas retornar uno de estos parametros para su uso en el codigo
+ */
 public enum PreciosProductos {
     COCACOLA(500, 1),
     SPRITE(500, 2),
-    FANTA(500, 5),
+    FANTA(500, 3),
     SNIKERS(400, 4),
     SUPER8(300, 5);
-    public int precio;
-    public int identificador;
+    private final int precio;
+    private final int identificador;
 
-    private PreciosProductos(int valor, int identificador) {
-        this.precio = valor;
+    /**
+     * Se asigna valor al precio e identificador del producot
+     * @param precio del producto
+     * @param identificador del producto
+     */
+    private PreciosProductos(int precio, int identificador) {
+        this.precio = precio;
         this.identificador = identificador;
     }
 
-    public int valor() {
+    /**
+     * @return el valor de la bebida
+     */
+    public int getPrecio() {
         return precio;
     }
 
+    /**
+     * @return el numero verificador del producto
+     */
     public int getIdentificador() {
         return identificador;
     }
